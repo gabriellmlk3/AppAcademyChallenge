fdea53557da87ac05104194d673791c631f77723
gabriellmlk3 <81688385+gabriellmlk3@users.noreply.github.com>
2021-05-20 22:53:36 -0300

    Add files via upload

--- Project/br/com/AppAcademy_Challenge/ArchiveRead/ReadArchive.java
+++ Project/br/com/AppAcademy_Challenge/ArchiveRead/ReadArchive.java
@@ -4,7 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class ReadArchive {
     public static void main(String[] args) {
@@ -18,192 +23,99 @@ public class ReadArchive {
         double qaCount = 0;
         double iosCount = 0;
         long idadeAverage = 0; 
-        String estados = null;
+        String estadosAtual = null;
+        String vagasAtual = null;
+        String nomesAtual = null;
+        int idadesAtual = 0;
+        int estadosCount = 0;
 
         if(csvFile.isFile()){
             try {
                 BufferedReader csvReader = new BufferedReader(new FileReader(csvFile));
+                List<String> estados = new ArrayList<>();
+                List<String> vagas = new ArrayList<>();
+                List<String> nomes = new ArrayList<>();
+                List<Integer> idades = new ArrayList<>();
+                
+                
                        
 
                 csvReader.readLine();
                 while((row = csvReader.readLine()) != null) {
-                    String[] data = row.split(";");
-                    estados = data[3];
-                    String vagas = data[1];                 
 
+                    String[] data = row.split(";");
+                    nomesAtual = data[0];
+                    vagasAtual = data[1];
+                    idadesAtual = Integer.parseInt(data[2].replace(" anos", ""));
+                    estadosAtual = data[3];
+
+                    
+                    nomes.add(nomesAtual);
+                    vagas.add(vagasAtual);
+                    idades.add(idadesAtual);
+                    estados.add(estadosAtual);
+                    //String vagas = data[1];                 
+                    
                         //Separa os candidatos por vaga e faz a contagem
-                    if (vagas.equals("Android")){                        
+                    if (row.contains("Android")){                        
                         androidCount++;                        
-                    } else if (vagas.equals("QA")) {
+                    } else if (row.contains("QA")) {
                         qaCount++; 
 
                             //Retira a String "anos" do dado recebido e transforma a string em inteiro
-                        int intIdade = Integer.parseInt(data[2].replace(" anos", ""));
+                        int intIdade = idadesAtual;
                         idadeCount += intIdade;
                         idadeAverage = idadeCount / Math.round(qaCount);                     
                         
-                    } else {
+                    } else if (row.contains("iOS")) {
                         iosCount++;                       
-                    } 
-
-                    
-
-                    switch (estados){
-                        case "SC": {
-
-                        }
-                        case  "AC":{
-
-                        }
-                        case  "AL":{
-
-                        }
-                        case  "AP":{
-
-                        }
-                        case  "AM":{
-
-                        }
-                        case  "BA":{
-
-                        }
-                        case  "CE":{
-
-                        }
-                        case  "ES":{
-
-                        }
-                        case  "GO":{
-
-                        }
-                        case  "MA":{
-
-                        }
-                        case  "MT":{
-
-                        }
-                        case  "MS":{
-
-                        }
-                        case  "MG":{
-
-                        }
-                        case  "PA":{
-
-                        }
-                        case  "PB":{
-
-                        }
-                        case  "PR":{
-
-                        }
-                        case  "PE":{
-
-                        }
-                        case  "PI":{
-
-                        }
-                        case  "RJ":{
-
-                        }
-                        case  "RN":{
-
-                        }
-                        case  "RS":{
-
-                        }
-                        case  "RO":{
-
-                        }
-                        case  "RR":{
-
-                        }                        
-                        case  "SP":{
-
-                        }
-                        case  "SE":{
-
-                        }
-                        case  "TO":{
-
-                        }
-                        case  "DF":{
-
-                        }
-
-                            
-
-                            
-
-                            
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-
-                            
-                        }
                     }
-
-                    lineCount++;                  
+                    lineCount++; 
+                                     
                 }
-                System.out.println(estados);
+                
+                Map<String, Integer> hm = new HashMap<String, Integer>();
+                
+                for (String i : estados) {
+                    Integer j = hm.get(i);
+                    hm.put(i, (j == null) ? 1 : j + 1);
+                }
 
                 Double androidPercent = androidCount / lineCount * 100;
                 Double qaPercent = qaCount / lineCount * 100;
                 Double iosPercent = iosCount / lineCount * 100;
 
-                System.out.println("Proporção de candidatos por vaga:");                
-                System.out.println("Android: " + Math.round(androidPercent) + "%");                
-                System.out.println("QA     : " + Math.round(iosPercent) + "%");
-                System.out.println("iOS    : " + Math.round(qaPercent) + "%");
-                System.out.println();
-                System.out.println("Idade média dos candidatos de QA: " + idadeAverage + " anos");
-
+               System.out.println("Proporção de candidatos por vaga:\n" +
+                "Android: " + Math.round(androidPercent) + "%\n" + 
+                "QA     : " + Math.round(iosPercent) + "%\n" + 
+                "iOS    : " + Math.round(qaPercent) + "%\n"); 
                 
+               System.out.println("Idade média dos candidatos de QA: " + idadeAverage + " anos\n");
 
+               System.out.println("Número de estados distintos presentes na lista: " + hm.size() + "\n");
+            
+               
+                
+               
+
+               
+               Map<String, Integer> map = new TreeMap<String, Integer>(hm);
+               Set set2 = map.entrySet();
+               Iterator iterator2 = set2.iterator();
+               
+               System.out.println("Rank dos 2 estados com menos ocorrências:\n");
+
+               for(int i = 1; i < 3; i++) {
+                   Map.Entry me2 = (Map.Entry)iterator2.next();
+                   System.out.print("#" + i + " " +  me2.getKey() + " - ");
+                   System.out.println(me2.getValue() + " candidato(s)");
+               }
+
+               csvReader.close();
             } catch (Exception e) {
                e.printStackTrace();
-            }
+               
+            } 
         }
     }
 }
